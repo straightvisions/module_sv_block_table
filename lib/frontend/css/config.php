386d22a6df225953ac989f295cdfb38836b82543
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-04-28 17:09:26 +0200

    IMPLEMENT CENTRALIZED CSS OUTPUT

--- lib/frontend/css/config.php
+++ lib/frontend/css/config.php
@@ -1,120 +1,13 @@
 <?php
-	// ##### SETTINGS #####
-
-	// Fetches all settings and creates new variables with the setting ID as name and setting data as value.
-	// This reduces the lines of code for the needed setting values.
-	foreach ( $script->get_parent()->get_settings() as $setting ) {
-		if ( $setting->get_type() !== false ) {
-			${ $setting->get_ID() } = $setting->get_data();
-		}
-	}
-
-	$properties					= array();
-
-	// Font
-	// @todo: double code
-	$value						= $font;
-	$font_family				= false;
-	$font_weight				= false;
-	foreach($value as $breakpoint => $val) {
-		if($val) {
-			$f							= $setting->get_parent()->get_module('sv_webfontloader')->get_font_by_label($val);
-			$font_family[$breakpoint]	= $f['family'];
-			$font_weight[$breakpoint]	= $f['weight'];
-		}else{
-			$font_family[$breakpoint]	= false;
-			$font_weight[$breakpoint]	= false;
-		}
-	}
-	if($font_family && (count(array_unique($font_family)) > 1 || array_unique($font_family)['mobile'] !== false)){
-		$properties['font-family']	= $setting->prepare_css_property_responsive($font_family,'',', sans-serif;');
-		$properties['font-weight']	= $setting->prepare_css_property_responsive($font_weight,'','');
-	}
-
-	if($font_size) {
-		$properties['font-size']	= $setting->prepare_css_property_responsive($font_size,'','px');
-	}
-
-	if($line_height) {
-		$properties['line-height']	= $setting->prepare_css_property_responsive($line_height);
-	}
-
-	if($text_color){
-		$properties['color']		= $setting->prepare_css_property_responsive($text_color,'rgba(',')');
-	}
-
-	// Margin
-	if($margin) {
-		$imploded		= false;
-		foreach($margin as $breakpoint => $val) {
-			$top = (isset($val['top']) && strlen($val['top']) > 0) ? $val['top'] : false;
-			$right = (isset($val['right']) && strlen($val['right']) > 0) ? $val['right'] : false;
-			$bottom = (isset($val['bottom']) && strlen($val['bottom']) > 0) ? $val['bottom'] : false;
-			$left = (isset($val['left']) && strlen($val['left']) > 0) ? $val['left'] : false;
-
-			if($top !== false || $right !== false || $bottom !== false || $left !== false) {
-				$imploded[$breakpoint] = $top . ' ' . $right . ' ' . $bottom . ' ' . $left;
-			}
-		}
-		if($imploded) {
-			$properties['margin'] = $setting->prepare_css_property_responsive($imploded, '', '');
-		}
-	}
-
-	// Padding
-	// @todo: same as margin, refactor to avoid doubled code
-	if($padding) {
-		$imploded		= false;
-		foreach($padding as $breakpoint => $val) {
-			$top = (isset($val['top']) && strlen($val['top']) > 0) ? $val['top'] : false;
-			$right = (isset($val['right']) && strlen($val['right']) > 0) ? $val['right'] : false;
-			$bottom = (isset($val['bottom']) && strlen($val['bottom']) > 0) ? $val['bottom'] : false;
-			$left = (isset($val['left']) && strlen($val['left']) > 0) ? $val['left'] : false;
-
-			if($top !== false || $right !== false || $bottom !== false || $left !== false) {
-				$imploded[$breakpoint] = $top . ' ' . $right . ' ' . $bottom . ' ' . $left;
-			}
-		}
-		if($imploded) {
-			$properties['padding'] = $setting->prepare_css_property_responsive($imploded, '', '');
-		}
-	}
-
-	// border
-	if($border) {
-		if($border['top_width']){
-			$val		= $border['top_width'].' '.$border['top_style'].' rgba('.$border['color'].')';
-			$properties['border-top'] = $setting->prepare_css_property_responsive($val, '', '');
-		}
-		if($border['right_width']){
-			$val		= $border['right_width'].' '.$border['right_style'].' rgba('.$border['color'].')';
-			$properties['border-right'] = $setting->prepare_css_property_responsive($val, '', '');
-		}
-		if($border['bottom_width']){
-			$val		= $border['bottom_width'].' '.$border['bottom_style'].' rgba('.$border['color'].')';
-			$properties['border-bottom'] = $setting->prepare_css_property_responsive($val, '', '');
-		}
-		if($border['left_width']){
-			$val		= $border['left_width'].' '.$border['left_style'].' rgba('.$border['color'].')';
-			$properties['border-left'] = $setting->prepare_css_property_responsive($val, '', '');
-		}
-
-		if($border['top_left_radius']+$border['top_right_radius']+$border['bottom_right_radius']+$border['bottom_left_radius']!==0) {
-			$border_radius = $border['top_left_radius'] . ' ' . $border['top_right_radius'] . ' ' . $border['bottom_right_radius'] . ' ' . $border['bottom_left_radius'];
-			$properties['border-radius'] = $setting->prepare_css_property_responsive($border_radius, '', '');
-		}
-	}
-
-	echo $setting->build_css(
+	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-table' : '.sv100_sv_content_wrapper article .wp-block-table',
-		$properties
+		array_merge(
+			$script->get_parent()->get_setting('font')->get_css_data('font-family'),
+			$script->get_parent()->get_setting('font_size')->get_css_data('font-size','','px'),
+			$script->get_parent()->get_setting('line_height')->get_css_data('line-height'),
+			$script->get_parent()->get_setting('text_color')->get_css_data(),
+			$script->get_parent()->get_setting('padding')->get_css_data('padding'),
+			$script->get_parent()->get_setting('margin')->get_css_data(),
+			$script->get_parent()->get_setting('border')->get_css_data()
+		)
 	);
-
-?>
-
-@media ( max-width: 782px ) {
-	.editor-styles-wrapper .wp-block-table,
-	.sv100_sv_content_wrapper article .wp-block-table {
-		font-size:var(--sv100_sv_common_font_size_small);
-	}
-}
