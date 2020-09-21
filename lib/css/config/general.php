035babedbe65b9f4a3bc913ed3de6a12a6238ecd
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-09-21 19:35:41 +0200

    MIGRATE TO V5.100

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -2,12 +2,12 @@
 	echo $_s->build_css(
 		is_admin() ? '.editor-styles-wrapper .wp-block-table' : '.sv100_sv_content_wrapper article .wp-block-table',
 		array_merge(
-			$script->get_parent()->get_setting('font')->get_css_data('font-family'),
-			$script->get_parent()->get_setting('font_size')->get_css_data('font-size','','px'),
-			$script->get_parent()->get_setting('line_height')->get_css_data('line-height'),
-			$script->get_parent()->get_setting('text_color')->get_css_data(),
-			$script->get_parent()->get_setting('padding')->get_css_data('padding'),
-			$script->get_parent()->get_setting('margin')->get_css_data(),
-			$script->get_parent()->get_setting('border')->get_css_data()
+			$module->get_setting('font')->get_css_data('font-family'),
+			$module->get_setting('font_size')->get_css_data('font-size','','px'),
+			$module->get_setting('line_height')->get_css_data('line-height'),
+			$module->get_setting('text_color')->get_css_data(),
+			$module->get_setting('padding')->get_css_data('padding'),
+			$module->get_setting('margin')->get_css_data(),
+			$module->get_setting('border')->get_css_data()
 		)
 	);
