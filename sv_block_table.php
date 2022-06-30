8cde11abd61b3b9e953a2cc8746002e53a26a14b
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-30 17:05:20 +0200

    remove default values

--- sv_block_table.php
+++ sv_block_table.php
@@ -28,7 +28,6 @@
 			$this->get_setting( 'font_size' )
 				->set_title( __( 'Font Size', 'sv100' ) )
 				->set_description( __( 'Font Size in pixel.', 'sv100' ) )
-				->set_default_value( 16 )
 				->set_is_responsive(true)
 				->load_type( 'number' );
 
@@ -40,7 +39,6 @@
 
 			$this->get_setting( 'text_color' )
 				->set_title( __( 'Text Color', 'sv100' ) )
-				->set_default_value( '30,30,30,1' )
 				->set_is_responsive(true)
 				->load_type( 'color' );
 
