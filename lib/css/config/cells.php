5fa26a3932f22339d8ca68b03fade383d19e3f78
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-26 16:25:37 +0200

    fix selector

--- lib/css/config/cells.php
+++ lib/css/config/cells.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-table td' : '.wp-block-table td',
+		'.wp-block-table td',
 		array_merge(
 			$module->get_setting('cells_padding')->get_css_data('padding'),
 			$module->get_setting('cells_margin')->get_css_data(),
--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-table' : '.wp-block-table',
+		'.wp-block-table',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family'),
 			$module->get_setting('font_size')->get_css_data('font-size','','px'),
